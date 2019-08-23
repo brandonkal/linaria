95ddd00c3e515d234757c6eff80c55184a605ac7
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-08-23 02:12:20 -0700

    feat: improve dynamic plugin perf

--- src/babel/dynamic-import-noop.ts
+++ src/babel/dynamic-import-noop.ts
@@ -3,12 +3,11 @@ import { NodePath } from '@babel/traverse';
 import syntax from '@babel/plugin-syntax-dynamic-import';
 
 export default function dynamic({ types: t }: { types: typeof types }) {
+  const noop = t.arrowFunctionExpression([], t.identifier('undefined'));
   return {
     inherits: syntax,
     visitor: {
       Import(path: NodePath<types.Import>) {
-        const noop = t.arrowFunctionExpression([], t.identifier('undefined'));
-
         path.parentPath.replaceWith(
           t.objectExpression([
             t.objectProperty(t.identifier('then'), noop),
