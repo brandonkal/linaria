408e62ecdf6e7ab89e0473f349f67f0d47b1f304
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-08-23 02:11:35 -0700

    feat: watch build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 COREFILES="src/core/**/,src/react/**,src/index.ts"
 
+WATCH='false'
+while getopts ':w' option; do
+  case "${option}" in
+    'w') WATCH='true';;
+    *) WATCH='false';;
+  esac
+done
+
 rm -rf lib
 
-# Build Core
-NODE_ENV=esm yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
-# Modify file name
-find ./lib -name "*.js" -exec bash -c 'mv "$1" "${1/.js/.mjs}"' -- {} \;
-find ./lib -name "*.js.map" -exec bash -c 'mv "$1" "${1/.js.map/.mjs.map}"' -- {} \;
-# Build Core commonjs
-yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
-# Build Node
-BABEL_ENV=node yarn babel src --out-dir lib --extensions '.ts' --ignore "**/__tests__/**,$COREFILES" --source-maps
+if $WATCH; then
+  # Build Node Watch mode
+  BABEL_ENV=node yarn babel src --out-dir lib --extensions '.ts' --ignore "**/__tests__/**" --source-maps --watch
+else
+  # Build Core
+  NODE_ENV=esm yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
+  # Modify file name
+  find ./lib -name "*.js" -exec bash -c 'mv "$1" "${1/.js/.mjs}"' -- {} \;
+  find ./lib -name "*.js.map" -exec bash -c 'mv "$1" "${1/.js.map/.mjs.map}"' -- {} \;
+  # Build Core commonjs
+  yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
+  # Build Node
+  BABEL_ENV=node yarn babel src --out-dir lib --extensions '.ts' --ignore "**/__tests__/**,$COREFILES" --source-maps
+fi
+
 
