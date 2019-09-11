a454513c5fdb7bc2d6c2622a1bd0873f6a8db122
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-09-10 23:16:10 -0700

    refactor: throwIfInvalid

--- src/babel/utils/throwIfInvalid.ts
+++ src/babel/utils/throwIfInvalid.ts
@@ -1,24 +1,25 @@
 import generator from '@babel/generator';
 import isSerializable from './isSerializable';
-import { Serializable } from '../types';
 import { NodePath } from '@babel/traverse';
 import isStyled from './isStyled';
 
-/** Throw if we can't handle the interpolated value */
-function throwIfInvalid(value: Function, ex: NodePath): void;
-function throwIfInvalid(value: string, ex: NodePath): void;
-function throwIfInvalid(value: number, ex: NodePath): void;
-function throwIfInvalid(value: Serializable, ex: NodePath): void;
-function throwIfInvalid(value: any, ex: NodePath): never;
-function throwIfInvalid(value: any, ex: NodePath): void | never {
-  if (
+function isValid(value: any) {
+  return !!(
     isStyled(value) ||
     typeof value === 'string' ||
     (typeof value === 'number' && Number.isFinite(value)) ||
-    (value && typeof value.cls === 'string') ||
-    isSerializable(value)
-  ) {
-    return;
+    (value &&
+      (typeof value.cls === 'string' ||
+        (Object.keys(value).length && isSerializable(value))))
+  );
+}
+
+/**
+ * Throw if we can't handle the interpolated value.
+ */
+function throwIfInvalid(value: any, ex: NodePath): void | never {
+  if (isValid(value)) {
+    return value;
   }
 
   // We can't use instanceof here so let's use duck typing
@@ -29,14 +30,14 @@ function throwIfInvalid(value: any, ex: NodePath): void | never {
     if (ex.buildCodeFrameError) {
       throw ex.buildCodeFrameError(errMsg);
     }
-    throw Error(errMsg);
+    throw new Error(errMsg);
   }
 
   const stringified =
     typeof value === 'object' ? JSON.stringify(value) : String(value);
 
   const errMsg =
-    `The expression evaluated to '${stringified}', which is probably a mistake.\n` +
+    `\nThe expression evaluated to '${stringified}', which is probably a mistake.\n` +
     `If you want it to be inserted into CSS, explicitly cast or transform the value to a string, e.g. - 'String(${
       generator(ex.node).code
     })'.`;
@@ -44,7 +45,7 @@ function throwIfInvalid(value: any, ex: NodePath): void | never {
   if (ex.buildCodeFrameError) {
     throw ex.buildCodeFrameError(errMsg);
   }
-  throw Error(errMsg);
+  throw new Error(errMsg);
 }
 
 export default throwIfInvalid;
