677b00bc39febe568adce95489cf65c95dbbe6d4
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-09-17 00:16:23 -0700

    refactor: config

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,12 +16,12 @@ if $WATCH; then
   BABEL_ENV=node yarn babel src --out-dir lib --extensions '.ts' --ignore "**/__tests__/**" --source-maps --watch
 else
   # Build Core
-  NODE_ENV=esm yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
+  BABEL_ENV=esm yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
   # Modify file name
   find ./lib -name "*.js" -exec bash -c 'mv "$1" "${1/.js/.mjs}"' -- {} \;
   find ./lib -name "*.js.map" -exec bash -c 'mv "$1" "${1/.js.map/.mjs.map}"' -- {} \;
   # Build Core commonjs
-  yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
+  BABEL_ENV=browser yarn babel src --out-dir lib --extensions '.ts' --only "$COREFILES" --source-maps
   # Build Node
   BABEL_ENV=node yarn babel src --out-dir lib --extensions '.ts' --ignore "**/__tests__/**,$COREFILES" --source-maps
 fi
